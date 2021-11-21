fb310f7fcabee9608d6e3ff50a2e1627561df369
Joonas <>
2021-11-21 14:32:26 +0200

    Build and zip gofiles to enable lambda creation

---
+++ build-and-zip-gofiles.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+lambdafolder="$PWD/lambdas/scripts"
+zipfolder="~/lambdas/zips"
+
+for folder in $lambdafolder/*
+do
+    for file in "$folder/*.go"
+    do
+        cd $folder
+        base=$(basename $file)
+        gobuildname=${base%%.*};   
+        GOOS=linux GOARCH=amd64 go build -o $folder/$gobuildname $folder/$base 
+        zip $gobuildname.zip $folder/$gobuildname
+        mv "$folder/$gobuildname.zip" $zipfolder
+    done
+done
